b389340ebaa94ca54ff96f70495076ca405f3247
josecarlos19 <carlojf0@gmail.com>
2023-11-04 18:43:39 -0300

    added notification for som products qtds

--- frontend/src/pages/products/index.tsx
+++ frontend/src/pages/products/index.tsx
@@ -1,10 +1,20 @@
 import Dashboard from "@/components/Dashboard";
 import useAxiosAuth from "@/lib/hooks/useAxiosAuth";
-import { Button, Col, Popconfirm, Row, Table, message } from "antd";
+import {
+  Button,
+  Col,
+  Popconfirm,
+  Row,
+  Table,
+  message,
+  notification,
+} from "antd";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { FaRegEye, FaTrashAlt } from "react-icons/fa";
+import { HiMiniArchiveBoxXMark } from "react-icons/hi2";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 import styles from "./styles.module.css";
 import { useRouter } from "next/router";
 
@@ -14,14 +24,34 @@ export default function Products() {
   const [products, setProducts] = useState([]);
   const [page, setPage] = useState(1);
   const [total, setTotal] = useState(0);
+  const [api, contextHolder] = notification.useNotification();
 
   const { data: session } = useSession();
+
+  const openNotification = (title: string, body: string, type: string) => {
+    api.open({
+      message: title,
+      duration: 0,
+      description: body,
+      icon:
+        type === "low_quantity" ? (
+          <FaMagnifyingGlass size={24} color={"#FFA500"} />
+        ) : (
+          <HiMiniArchiveBoxXMark size={24} color={"#ff1e00"} />
+        ),
+
+      onClick: () => {
+        router.push("/acquirements/create");
+      },
+    });
+  };
 
   interface Product {
     id: string;
     name: string;
     description: string;
     quantity: number;
+    min_quantity: number;
     price: number;
     category: string;
     type: string;
@@ -46,6 +76,26 @@ export default function Products() {
     }
   }, [session?.user.accessToken]);
 
+  useEffect(() => {
+    products.forEach((item: Product) => {
+      if (item.quantity < item.min_quantity && item.quantity > 0) {
+        openNotification(
+          `Atenção para ${item.name}!`,
+          `O produto ${item.name} está com a quantidade abaixo do mínimo.`,
+          "low_quantity",
+        );
+      }
+
+      if (item.quantity === 0) {
+        openNotification(
+          `${item.name} sem estoque!`,
+          `O produto ${item.name} está sem estoque.`,
+          "out_of_stock",
+        );
+      }
+    });
+  }, [products]);
+
   const columns = [
     {
       title: "Código",
@@ -64,7 +114,14 @@ export default function Products() {
       title: "Descrição",
       dataIndex: "description",
       key: "description",
-      width: "10%",
+      width: "9%",
+      responsive: ["md"],
+    },
+    {
+      title: "Quantidade mínima",
+      dataIndex: "min_quantity",
+      key: "min_quantity",
+      width: "1%",
       responsive: ["md"],
     },
     {
@@ -130,6 +187,7 @@ export default function Products() {
 
   return (
     <Dashboard>
+      {contextHolder}
       <Row justify={"space-between"} align={"middle"}>
         <Col>
           <h1>Produtos</h1>
