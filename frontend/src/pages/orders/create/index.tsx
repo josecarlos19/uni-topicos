01ab27ddea5bf8843201ae4d0f18cf6e3edb56ab
josecarlos19 <carlojf0@gmail.com>
2023-11-04 15:47:35 -0300

    fx: refactor create order

--- frontend/src/pages/orders/create/index.tsx
+++ frontend/src/pages/orders/create/index.tsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react";
 import {
   Button,
   Col,
-  Form,
-  Input,
   Row,
   Select,
   notification,
@@ -13,21 +11,21 @@ import {
   InputNumber,
   Divider,
 } from "antd";
-import { FaCheck } from "react-icons/fa";
+import { FaCheck, FaTrash } from "react-icons/fa";
 import Dashboard from "@/components/Dashboard";
 import useAxiosAuth from "@/lib/hooks/useAxiosAuth";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import moment from "moment";
 
-interface Products {
+interface OrderProduct {
   id: number;
+  name?: string;
   quantity: number;
 }
 
 interface Order {
   customer_id: number;
-  products: Products[];
+  products: OrderProduct[];
 }
 
 interface CustomerLabel {
@@ -49,11 +47,15 @@ interface Product {
 }
 
 export default function Show() {
-  const [order, setOrder] = React.useState({} as Order);
-  const [products, setProducts] = React.useState<Product[]>([]);
-  const [orderProducts, setOrderProducts] = React.useState<Products[]>([]);
+  const [order, setOrder] = useState<Order>({} as Order);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [orderProducts, setOrderProducts] = useState<OrderProduct[]>([]);
+  const [currentProduct, setCurrentProduct] = useState<OrderProduct>(
+    {} as OrderProduct,
+  );
+  const [selected, setSelected] = useState<any>(null);
 
-  const [customers, setCustomers] = React.useState<CustomerLabel[]>([]);
+  const [customers, setCustomers] = useState<CustomerLabel[]>([]);
 
   const [isLoad, setIsLoad] = useState(false);
 
@@ -93,6 +95,8 @@ export default function Show() {
   }
 
   async function submit() {
+    order!.products = orderProducts;
+
     setIsLoad(true);
     await axios.post("/financial/sell", order);
     setIsLoad(false);
@@ -104,40 +108,34 @@ export default function Show() {
   }
 
   const handleChangeCustomer = (value: string) => {
-    setOrder({ ...order, customer_id: Number(value) });
+    setOrder({ ...order, customer_id: +value });
   };
 
-  const handleSelectOrderProducts = (selectedValues: string[]) => {
-    setOrderProducts([]);
-    const productsAux = [];
-    for (let index = 0; index < selectedValues.length; index++) {
-      const element = selectedValues[index];
-      productsAux.push({
-        id: Number(element),
+  const handleSelectOrderProduct = (selectedProductId: number) => {
+    const currentProduct = products.find(
+      (product: Product) => product.id == selectedProductId,
+    );
+
+    setSelected(selectedProductId);
+
+    if (currentProduct) {
+      setCurrentProduct({
+        id: currentProduct.id,
+        name: currentProduct.name,
         quantity: 1,
-        name: products.find((product) => product.id === Number(element))?.name,
       });
     }
-
-    setOrderProducts(productsAux);
-
-    console.log(orderProducts);
   };
 
-  const handleOrderProductQuantity = (value: number, id: number) => {
-    const productsAux = [...orderProducts];
-    const productIndex = productsAux.findIndex((product) => product.id === id);
-    productsAux[productIndex].quantity = value;
-    setOrderProducts(productsAux);
+  const handleOrderProductQuantity = (quantity: any) => {
+    setCurrentProduct({ ...currentProduct, quantity: quantity });
   };
 
-  const deleteOrderProduct = (value: string) => {
-    const productsAux = [...orderProducts];
-    const productIndex = productsAux.findIndex(
-      (product) => product.id === Number(value),
-    );
-    productsAux.splice(productIndex, 1);
-    setOrderProducts(productsAux);
+  const handleAddProductToOrder = () => {
+    setOrderProducts([...orderProducts, currentProduct]);
+
+    setSelected(null);
+    setCurrentProduct({} as OrderProduct);
   };
 
   useEffect(() => {
@@ -147,169 +145,222 @@ export default function Show() {
     }
   }, [session?.user.accessToken]);
 
-  useEffect(() => {
-    setOrder({
-      ...order,
-      products: orderProducts.map((product) => {
-        return {
-          id: product.id,
-          quantity: product.quantity,
-        };
-      }),
-    });
-    console.log(order);
-  }, [orderProducts]);
-
   return (
     <Dashboard>
       <div>
         {contextHolder}
         <h1>Realizar venda</h1>
-
         <Row justify={"center"}>
-          <Form
-            labelCol={{ span: "auto" }}
-            wrapperCol={{ span: "auto" }}
-            layout="vertical"
-            style={{ minWidth: 600, maxWidth: 600 }}
-          >
-            <Row gutter={16}>
-              <Col span={12}>
-                <Form.Item label="Cliente">
-                  <Select
-                    placeholder="Selecione um(a) cliente"
-                    optionFilterProp="children"
-                    onChange={handleChangeCustomer}
-                    options={customers}
-                    key={customers[0]?.value}
-                  />
-                </Form.Item>
-              </Col>
-              <Col span={12}>
-                <Form.Item label="Data da ordem">
-                  <Input
-                    type=""
-                    value={moment().format("DD/MM/YYYY")}
-                    disabled={true}
-                  />
-                </Form.Item>
+          <Col style={{ minWidth: 600, maxWidth: 600 }}>
+            <Row>
+              <Col>
+                <label>Cliente</label>
+                <Select
+                  style={{ width: "100%", paddingTop: "5px" }}
+                  placeholder="Selecione um(a) cliente"
+                  optionFilterProp="children"
+                  onChange={handleChangeCustomer}
+                  options={customers}
+                  key={customers[0]?.value}
+                />
               </Col>
             </Row>
-            <Row>
-              <Col span={24}>
-                <Form.Item label="Produtos">
-                  <Select
-                    style={{ width: "100%" }}
-                    mode="multiple"
-                    allowClear
-                    placeholder="Selecione os produtos"
-                    optionFilterProp="children"
-                    onChange={(value) => handleSelectOrderProducts(value)}
-                    onDeselect={(value) => {
-                      deleteOrderProduct(value);
-                    }}
-                    options={products.map((product) => {
+
+            <Row style={{ paddingTop: "10px" }} gutter={16}>
+              <Col span={10}>
+                <label>Produtos</label>
+                <Select
+                  style={{ width: "100%", paddingTop: "5px" }}
+                  value={selected}
+                  placeholder="Selecione o produto"
+                  optionFilterProp="children"
+                  onChange={handleSelectOrderProduct}
+                  options={products
+                    .filter((product) => product.quantity > 0)
+                    .map((product) => {
                       return {
                         value: String(product.id),
                         label: product.name + " - " + "R$ " + product.price,
                       };
+                    })
+                    .filter((product) => {
+                      return (
+                        orderProducts.find(
+                          (orderProduct) => orderProduct.id == +product.value,
+                        ) === undefined
+                      );
                     })}
-                  />
-                </Form.Item>
+                />
               </Col>
+              <Col span={6}>
+                <label>Quantidade</label>
+                <InputNumber
+                  style={{ width: "100%", paddingTop: "5px" }}
+                  disabled={currentProduct.id === undefined}
+                  min={1}
+                  max={
+                    products.find((product) => product.id === currentProduct.id)
+                      ?.quantity
+                  }
+                  defaultValue={0}
+                  addonAfter="Un"
+                  onChange={handleOrderProductQuantity}
+                  value={currentProduct.quantity}
+                />
+              </Col>
+              <Col span={6}>
+                <label>Subtotal</label>
+
+                <InputNumber
+                  style={{ width: "100%", paddingTop: "5px" }}
+                  disabled={true}
+                  value={
+                    (Number(
+                      products.find(
+                        (product: Product) => product.id == currentProduct.id,
+                      )?.price,
+                    ) *
+                      currentProduct.quantity) |
+                    0
+                  }
+                  addonBefore="R$"
+                />
+              </Col>
+              <Row>
+                <Col span={2}>
+                  <Button
+                    type="primary"
+                    style={{ marginLeft: "9px", marginTop: "15px" }}
+                    onClick={handleAddProductToOrder}
+                    disabled={currentProduct.id === undefined}
+                  >
+                    Adicionar produto
+                  </Button>
+                </Col>
+              </Row>
             </Row>
-            <Row>
-              <label>Produtos selecionados: {orderProducts.length}</label>
+
+            <Row style={{ paddingTop: "20px" }}>
+              <label style={{ paddingBottom: "10px" }}>
+                Produtos selecionados: {orderProducts.length}
+              </label>
 
               <Col span={24} style={{ overflow: "auto", height: "310px" }}>
-                <Form.Item style={{ paddingTop: "8px" }}>
-                  <List
-                    grid={{ column: 2 }}
-                    bordered
-                    footer={
-                      <div>
-                        <Divider />
-                        <Space>
-                          <label>Total:</label>
-                          <label>
-                            R${" "}
-                            {orderProducts
-                              .reduce(
-                                (acc, cur) =>
-                                  acc +
-                                  Number(
-                                    products.find(
-                                      (product) => product.id === cur.id,
-                                    )?.price,
-                                  ) *
-                                    cur.quantity,
-                                0,
-                              )
-                              .toFixed(2)}
-                          </label>
-                        </Space>
-                      </div>
-                    }
-                    dataSource={orderProducts}
-                    renderItem={(item: any) => (
-                      <List.Item>
-                        <Card
-                          style={{ marginTop: "10px" }}
-                          size="small"
-                          title={item.name}
-                          key={item.id}
-                        >
-                          <InputNumber
-                            style={{ width: "100%" }}
-                            min={1}
-                            max={
-                              products.find((product) => product.id === item.id)
-                                ?.quantity
-                            }
-                            defaultValue={item.quantity}
-                            onChange={(value) => {
-                              handleOrderProductQuantity(value, item.id);
-                            }}
-                            addonAfter="Un"
-                          />
+                <List
+                  grid={{ column: 2 }}
+                  bordered
+                  footer={
+                    <div>
+                      <Divider />
+                      <Space>
+                        <label>Total:</label>
+                        <label>
+                          R${" "}
+                          {orderProducts
+                            .reduce(
+                              (acc, cur) =>
+                                acc +
+                                Number(
+                                  products.find(
+                                    (product) => product.id === cur.id,
+                                  )?.price,
+                                ) *
+                                  cur.quantity,
+                              0,
+                            )
+                            .toFixed(2)}
+                        </label>
+                      </Space>
+                    </div>
+                  }
+                  dataSource={orderProducts}
+                  renderItem={(item: any) => (
+                    <Card
+                      style={{
+                        marginTop: "10px",
+                        marginBottom: "10px",
+                        marginRight: "5px",
+                        marginLeft: "5px",
+                      }}
+                      size="small"
+                      title={item.name}
+                      key={item.id}
+                    >
+                      <InputNumber
+                        style={{ width: "100%" }}
+                        min={1}
+                        disabled={true}
+                        max={
+                          products.find((product) => product.id === item.id)
+                            ?.quantity
+                        }
+                        defaultValue={item.quantity}
+                        addonAfter="Un"
+                      />
 
-                          <label
-                            style={{
-                              marginTop: "10px",
-                              display: "block",
-                              textAlign: "right",
-                            }}
-                          >
-                            R${" "}
-                            {(
-                              Number(
-                                products.find(
-                                  (product) => product.id === item.id,
-                                )?.price,
-                              ) * item.quantity
-                            ).toFixed(2)}
-                          </label>
+                      <label
+                        style={{
+                          marginTop: "10px",
+                          display: "block",
+                          textAlign: "right",
+                        }}
+                      >
+                        Preço:{" "}
+                        {
+                          products.find((product) => product.id === item.id)!
+                            .price
+                        }
+                      </label>
 
-                          <label
-                            style={{
-                              marginTop: "10px",
-                              display: "block",
-                              textAlign: "right",
-                            }}
-                          >
-                            Disponível:{" "}
-                            {products.find((product) => product.id === item.id)!
-                              .quantity - item.quantity}
-                          </label>
-                        </Card>
-                      </List.Item>
-                    )}
-                  />
-                </Form.Item>
+                      <label
+                        style={{
+                          marginTop: "10px",
+                          display: "block",
+                          textAlign: "right",
+                        }}
+                      >
+                        Subtotal R${" "}
+                        {(
+                          Number(
+                            products.find((product) => product.id === item.id)
+                              ?.price,
+                          ) * item.quantity
+                        ).toFixed(2)}
+                      </label>
+
+                      <label
+                        style={{
+                          marginTop: "10px",
+                          display: "block",
+                          textAlign: "right",
+                        }}
+                      >
+                        Disponível:{" "}
+                        {products.find((product) => product.id === item.id)!
+                          .quantity - item.quantity}
+                      </label>
+                      <Button
+                        style={{
+                          backgroundColor: "transparent",
+                          border: "none",
+                          boxShadow: "none",
+                        }}
+                        onClick={() =>
+                          setOrderProducts(
+                            orderProducts.filter(
+                              (orderProduct) => orderProduct.id !== item.id,
+                            ),
+                          )
+                        }
+                      >
+                        <FaTrash size={20} color={"#ff0000"} />
+                      </Button>
+                    </Card>
+                  )}
+                />
               </Col>
             </Row>
 
-            <br />
             <Row justify={"space-between"}>
               <Col>
                 <Button
@@ -323,20 +374,16 @@ export default function Show() {
               </Col>
               <Col>
                 <Button
-                  onClick={() => submit()}
+                  onClick={submit}
                   type="primary"
                   size={"middle"}
-                  disabled={
-                    isLoad ||
-                    order.customer_id === undefined ||
-                    order.products.length === 0
-                  }
+                  disabled={isLoad || order.customer_id === undefined}
                 >
                   Confirmar venda
                 </Button>
               </Col>
             </Row>
-          </Form>
+          </Col>
         </Row>
       </div>
     </Dashboard>
